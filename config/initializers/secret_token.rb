35bdba0bda3b219772d1818fb129b236e197fbf8
cis-deepesh <deepesh.k@cisinlabs.com>
2016-04-30 09:05:12 +0530

    Adding rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitSession::Application.config.secret_token = 'e0831c2d433b21d55585afc1152fbacaba741b1804b54e906dcd5be41336c5cdc080baeeffd604161247bd60c307755fa082b732326c9b0acd762ab70da68455'
